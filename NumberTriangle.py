792aa7757db27b1fa560c3ba44a9ba4053a202bd
ankitgujre <ankitgujre374@gmail.com>
2026-01-29 20:09:30 +0530

    Alphabet pattern

--- NumberTriangle.py
+++ NumberTriangle.py
@@ -7,5 +7,12 @@ st = ""
 
     
 
-for i in range(1, n+1):
-    print(str(i) * i)
+# for i in range(1, n+1):
+#     print(str(i) * i)
+st = 1
+    
+for row in range(1,6):
+    for col in range(1,6):
+        if col<=row:
+            print(st, end="")
+            st += 1
---
+++ alphabetPattern.py
@@ -0,0 +1,11 @@
+n = 4
+st = ""
+
+# for i in range(n):
+#     st += chr(65 + i)
+#     print(st + " "*(n - i))
+    
+    
+for i in range(1, n+1):
+    print(chr(64+i) * i)
+    
